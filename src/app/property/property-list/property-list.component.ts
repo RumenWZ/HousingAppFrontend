e55a2cfbfa9743ffc50b2425675d3bb3b23999f9
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2023-03-10 15:50:47 +0200

    implement dependancy injection for data

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,35 +1,29 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HousingService } from 'src/app/services/housing.service';
+
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
-  Properties: Array<any> = [
-    {
-    "Id": 1,
-    "Name": "Barla House",
-    "Type": "House",
-    "Price": 12000,
-  },
-  {
-    "Id": 2,
-    "Name": "Erose Villa",
-    "Type": "House",
-    "Price": 15000,
-  },
-  {
-    "Id": 3,
-    "Name": "Jul Villa",
-    "Type": "House",
-    "Price": 23000,
-  },
-  {
-    "Id": 4,
-    "Name": "Postmyre Mansion",
-    "Type": "House",
-    "Price": 31000,
+export class PropertyListComponent{
+  Properties: any;
+
+  constructor(private housingService: HousingService) {
+
+
   }
-]
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+      data=>{
+        this.Properties=data;
+        console.log(data);
+      }, error => {
+        console.log(error);
+      }
+    );
+  }
 }
