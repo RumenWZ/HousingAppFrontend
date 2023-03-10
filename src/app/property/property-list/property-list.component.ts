eaafc87d23884184fe899c601105995277bebd2b
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2023-03-10 15:26:42 +0200

    list property cards with css styling

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent {
-
+  Properties: Array<any> = [
+    {
+    "Id": 1,
+    "Name": "Barla House",
+    "Type": "House",
+    "Price": 12000,
+  },
+  {
+    "Id": 2,
+    "Name": "Erose Villa",
+    "Type": "House",
+    "Price": 15000,
+  },
+  {
+    "Id": 3,
+    "Name": "Jul Villa",
+    "Type": "House",
+    "Price": 23000,
+  },
+  {
+    "Id": 4,
+    "Name": "Postmyre Mansion",
+    "Type": "House",
+    "Price": 31000,
+  }
+]
 }
