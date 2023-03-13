c54e2cff7a61dba9156ba02523234cdd216dda3b
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2023-03-13 18:26:03 +0200

    register/login/logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,6 +43,7 @@ const appRoutes: Routes = [
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent {
+  registerForm!: FormGroup;
+
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      userName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, Validators.required),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    }, this.passwordMatchingValidator);
+  }
+
+  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null {
+    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null :
+      { notmatched: true }
+  };
+
+  get userName() {
+    return this.registerForm.get('userName') as FormControl;
+  }
+
+  get email() {
+    return this.registerForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registerForm.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword') as FormControl;
+  }
+
+  get mobile() {
+    return this.registerForm.get('mobile') as FormControl;
+  }
+
+  onSubmit(){
+    console.log(this.registerForm);
+  }
 
 }
